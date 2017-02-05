10d9048c987424cdbdf4c223bde6e903c0caed99
Kemele M. Endris <keme686@gmail.com>
2017-02-05 15:33:55 +0100

    example data for test added

--- test_query.py
+++ test_query.py
@@ -7,12 +7,13 @@ if __name__ == "__main__":
         prefix bsbm: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/>
         prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
         prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        SELECT DISTINCT ?product ?productfeature WHERE {
-                ?product bsbm:productFeature <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature34439>.
+        SELECT DISTINCT ?product WHERE {
+                ?product bsbm:productFeature <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature34439> .
                 ?product bsbm:productPropertyNumeric1 ?value1 .
                 ?product bsbm:productFeature <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature892> .
-                ?product <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductType1446>.
-                ?product bsbm:productFeature ?productfeature
+                ?product <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductType1446>
+        FILTER ((?value1 > 136))
+
         }
             limit 10
     '''
--- test_rest.py
+++ test_rest.py
@@ -4,19 +4,19 @@ import httplib
 
 if __name__ == "__main__":
     query = '''
-        prefix bsbm-inst: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/>
-        prefix bsbm: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/>
-        prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        SELECT DISTINCT ?product WHERE {
-                ?product bsbm:productFeature <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature34439>.
-                ?product bsbm:productPropertyNumeric1 ?value1 .
-                ?product bsbm:productFeature <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature892> .
-                ?product <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductType1446>
-        }
-            limit 10
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX rev: <http://purl.org/stuff/rev#>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        PREFIX bsbm: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/>
+        PREFIX dc: <http://purl.org/dc/elements/1.1/>
+        SELECT ?reviewer ?revPublisher
+        WHERE {
+            ?reviewer a <http://xmlns.com/foaf/0.1/Person>.
+            ?reviewer dc:date "2008-09-05" .
+            ?reviewer dc:publisher ?revPublisher
+        } limit 10
     '''
-    params = urllib.urlencode({'query': query, 'molecule': 'http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/Product'})
+    params = urllib.urlencode({'query': query, 'molecule': 'http://xmlns.com/foaf/0.1/Person'})
     headers = {"Accept": "*/*", "Referer": 'http://localhost:27001/', "Host": 'localhost:27001'}
     # Establish connection and get response from server.
     conn = httplib.HTTPConnection('localhost:27001')
